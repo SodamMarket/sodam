98edcb674c1ae527ec34c2b46ddf6fcfb335a8cf
temtamK <temtamk@kakao.com>
2020-02-22 23:34:06 +0900

    [ADD]Follow

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 	"sodam/internal/service"
+
+	"github.com/matryer/way"
 )
 
 type createUserInput struct {
@@ -36,5 +38,34 @@ func (h *handler) createUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusNoContent)
+}
 
+// 팔로우 핸들러
+func (h *handler) toggleFollow(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	username := way.Param(ctx, "username")
+
+	out, err := h.ToggleFollow(ctx, username)
+	// 인증되지 않은 유저일 때
+	if err == service.ErrUnauthenticated {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+	// 유저 이름이 맞지 않을 때
+	if err == service.ErrInvalidUsername {
+		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+		return
+	}
+	// 유저를 찾을 수 없을 때
+	if err == service.ErrUserNotFound {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	// 스스소를 팔로우 할 때
+	if err != nil {
+		respondError(w, err)
+		return
+	}
+
+	respond(w, out, http.StatusOK)
 }
--- internal/service/user.go
+++ internal/service/user.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"fmt"
 	"regexp"
@@ -22,12 +23,21 @@ var (
 	ErrEmailTaken = errors.New("email taken")
 	// ErrUsernameTaken used when there is already an user registered with that username.
 	ErrUsernameTaken = errors.New("username taken")
+	// ErrForbiddenFollow is used when you try to following yourself
+	ErrForbiddenFollow = errors.New("cannot follow yourself")
 )
 
 //User Model
 type User struct {
 	ID       int64  `json:"id,omitempty"`
 	UserName string `json:"user_name,omitempty"`
+}
+
+//팔로워 카운트
+//ToggleFollowOutput response
+type ToggleFollowOutput struct {
+	Following      bool `json:"following,omitempty"`
+	FollowersCount int  `json:"followers_count,omitempty"`
 }
 
 // CreateUser inserts a user int the database.
@@ -61,4 +71,114 @@ func (s *Service) CreateUser(ctx context.Context, email, username string) error 
 	}
 
 	return nil
+}
+
+// 팔로워 이름을 받는 기능
+// ToggleFollow between Two users.
+func (s *Service) ToggleFollow(ctx context.Context, username string) (ToggleFollowOutput, error) {
+	var out ToggleFollowOutput
+	// 유저 아이디 체크
+	// check for the auth user in the context
+	followerID, ok := ctx.Value(KeyAuthUserID).(int64)
+	if !ok {
+		return out, ErrUnauthenticated
+	}
+
+	// 유저 이름 확인
+	// validate username
+	username = strings.TrimSpace(username)
+	if !rxUsername.MatchString(username) {
+		return out, ErrInvalidUsername
+	}
+
+	// 쿼리를 트랜잭션으로 보내기
+	// for queries to use transaction
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return out, fmt.Errorf("could not begin tx: %v", err)
+	}
+
+	defer tx.Rollback()
+
+	// 유저 이름으로부터 유저 아이디 받아오기
+	// Get the actual user ID from the username
+	var followeeID int64
+	query := "SELECT id FROM users WHERE username = $1"
+	tx.QueryRowContext(ctx, query, username).Scan(&followeeID)
+	if err == sql.ErrNoRows {
+		return out, ErrUserNotFound
+	}
+
+	if err != nil {
+		return out, fmt.Errorf("could not query select user id from followee username: %v", err)
+	}
+
+	if followeeID == followerID {
+		return out, ErrForbiddenFollow
+	}
+
+	// check overlap ID
+	// 팔로워 중복 확인
+	query = "SELECT EXISTS (SELECT 1 FROM follows WHERE follower_id = $1 AND followee_id = $2)"
+	if err = tx.QueryRowContext(ctx, query, followerID, followeeID).Scan(&out.Following); err != nil {
+		return out, fmt.Errorf("could not query select existance of follow: %v", err)
+	}
+
+	// 팔로우를 두 번 눌렀을 때의 액션
+	// Action when pressed follow button twice
+
+	// 팔로우 취소와 팔로워 수 감소
+	// Cancle follow and decrease follow
+	if out.Following {
+		//취소
+		//cancle
+		query = "DELETE FROM follows WHERE follower_id = $1 AND followee_id = $2"
+		if _, err = tx.ExecContext(ctx, query, followerID, followeeID); err != nil {
+			return out, fmt.Errorf("could not delete follow : %v", err)
+		}
+		//팔로위 감소
+		//followee
+		query = "UPDATE users SET followees_count = followees_count - 1 WHERE id = $1"
+		if _, err = tx.ExecContext(ctx, query, followerID); err != nil {
+			return out, fmt.Errorf("could not update follower followees count (-): %v", err)
+		}
+		//팔로워 감소
+		//follower
+		query = "UPDATE users SET followers_count = followers_count - 1 WHERE id = $1 RETURNING followers_count"
+		if err = tx.QueryRowContext(ctx, query, followeeID).Scan(&out.FollowersCount); err != nil {
+			return out, fmt.Errorf("could not update followee followers count (-): %v", err)
+		}
+	} else {
+		// 팔로우와 팔로워 증가
+		// increment follow and follower
+		query = "INSERT INTO follows (follower_id, followee_id) VALUES ($1, $2)"
+		if _, err = tx.ExecContext(ctx, query, followerID, followeeID); err != nil {
+			return out, fmt.Errorf("could not insert follow : %v", err)
+		}
+		// 팔로위 증가
+		// followee increment
+		query = "UPDATE users SET followees_count = followees_count + 1 WHERE id = $1"
+		if _, err = tx.ExecContext(ctx, query, followerID); err != nil {
+			return out, fmt.Errorf("could not update follower followees count (+): %v", err)
+		}
+		// 팔로워 증가
+		// follower increment
+		query = "UPDATE users SET followers_count = followers_count + 1 WHERE id = $1 RETURNING followers_count"
+		if err = tx.QueryRowContext(ctx, query, followeeID).Scan(&out.FollowersCount); err != nil {
+			return out, fmt.Errorf("could not update followee followers count (+): %v", err)
+		}
+	}
+
+	if err = tx.Commit(); err != nil {
+		return out, fmt.Errorf("could not commit toggle follow : %v", err)
+	}
+
+	out.Following = !out.Following
+	// 팔로잉을 끊었을 시 더이상 알림 받지 않음
+	// Dispatch a notification
+	if out.Following {
+		// TODO: notify followw
+	}
+
+	return out, nil
 }
